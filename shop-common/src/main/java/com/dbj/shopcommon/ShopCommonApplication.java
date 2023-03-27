4bad495276a1a8674ab8532558e938ff5ac9aa46
dbj2001 <1264440273@qq.com>
2023-03-27 23:30:43 +0800

    Initial commit

---
+++ shop-common/src/main/java/com/dbj/shopcommon/ShopCommonApplication.java
@@ -0,0 +1,13 @@
+package com.dbj.shopcommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopCommonApplication.class, args);
+    }
+
+}
---
+++ shop-common/src/test/java/com/dbj/shopcommon/ShopCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbj.shopcommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
