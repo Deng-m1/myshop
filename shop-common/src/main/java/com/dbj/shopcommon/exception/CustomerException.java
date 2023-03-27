ea5417466b67dd1ad465f0f2376f4ae1e1bd44ca
dbj2001 <1264440273@qq.com>
2023-03-27 23:36:57 +0800

    共同变量，分布式id生成

---
+++ shop-common/src/main/java/com/dbj/shopcommon/exception/CustomerException.java
@@ -0,0 +1,15 @@
+package com.dbj.shopcommon.exception;
+
+import com.dbj.shopcommon.constant.ShopCode;
+
+/**
+ * 自定义异常
+ */
+public class CustomerException extends RuntimeException{
+
+    private ShopCode shopCode;
+
+    public CustomerException(ShopCode shopCode) {
+        this.shopCode = shopCode;
+    }
+}
