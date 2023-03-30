6555b4992eb2190ae3aa4ca8f90914b05bfb9b44
dbj2001 <1264440273@qq.com>
2023-03-30 23:43:36 +0800

    用户服务1.0

---
+++ shop-coupon-service/src/test/java/com/dbj/shopcouponservice/ShopCoupouServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbj.shopcouponservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCoupouServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ shop-goods-service/src/test/java/com/dbj/shopgoodsservice/ShopGoodsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbj.shopgoodsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopGoodsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
